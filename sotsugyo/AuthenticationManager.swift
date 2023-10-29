80ff053dec3274cd5fe382f89fd71097ab84b8c5
sakihosonuma <chipdale411@gmail.com>
2023-10-29 16:38:49 +0900

    a

--- sotsugyo/AuthenticationManager.swift
+++ sotsugyo/AuthenticationManager.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+@Observable class AuthenticationManager {
+    private(set) var isSignIn: Bool = false
+    private var handle: AuthStateDidChangeListenerHandle!
+    
+    init() {
+        // ここで認証状態の変化を監視する（リスナー）
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let _ = user {
+                print("Sign-in")
+                self.isSignIn = true
+            } else {
+                print("Sign-out")
+                self.isSignIn = false
+            }
+        }
+    }
+    
+    deinit {
+        // ここで認証状態の変化の監視を解除する
+        Auth.auth().removeStateDidChangeListener(handle)
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error")
+        }
+    }
+}
--- sotsugyo/LoginView.swift
+++ sotsugyo/LoginView.swift
@@ -6,13 +6,73 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
+import FirebaseOAuthUI
+import FirebaseEmailAuthUI
+import FirebaseFirestore
 
-struct loginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LoginView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let authUI = FUIAuth.defaultAuthUI()!
+        // サポートするログイン方法を構成
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(authUI: authUI),
+            FUIOAuth.appleAuthProvider(),
+            FUIEmailAuth()
+        ]
+        authUI.providers = providers
+        
+        // FirebaseUIを表示する
+        let authViewController = authUI.authViewController()
+        
+        return authViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+        // 処理なし
+    }
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        // Check if there was an error
+        if let user = authDataResult?.user {
+            let email = user.email
+            let name = user.displayName
+          
+            Task{
+                do{
+                    
+        
+                       
+                        try await self.saveUserData(email: email, name: name)
+                        print("新規登録成功！")
+                        
+                    
+                
+                }catch{
+                    print(error)
+                }
+            }
+        }
+        if let error = error {
+            print("Error signing in: \(error.localizedDescription)")
+            return
+            
+        }
+    }
+    func saveUserData(email: String?, name: String?)async throws {
+        let db = Firestore.firestore()
+        let uid = Auth.auth().currentUser?.uid
+        print("uid！")
+        try await db.collection("users").document(uid ?? "").collection("personal").document("info").setData([
+            "uid": uid ?? "uid:Error",
+            "email": email ?? "email:Error",
+            "name": name ?? "name:Error",
+        ])
+      
+     
     }
 }
 
-#Preview {
-    loginView()
-}
+
