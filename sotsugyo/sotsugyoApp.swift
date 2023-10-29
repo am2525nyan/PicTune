438e9227e2e2f99e1f65b41687386cbc45a10003
sakihosonuma <chipdale411@gmail.com>
2023-10-29 13:50:32 +0900

    Initial Commit

---
+++ sotsugyo/sotsugyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sotsugyoApp.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/10/29.
+//
+
+import SwiftUI
+
+@main
+struct sotsugyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
