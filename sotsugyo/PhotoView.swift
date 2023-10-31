2b6cbbc1e895acacf263e8ed40741dfc799ab809
sakihosonuma <chipdale411@gmail.com>
2023-10-31 22:20:51 +0900

    カメラ画面に推移できるようになったよ📷

---
+++ sotsugyo/PhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/10/31.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoView()
+}
