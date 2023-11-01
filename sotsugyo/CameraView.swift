ae39a46ec73d5db4b1b79df225be3626a01b72b7
sakihosonuma <chipdale411@gmail.com>
2023-11-01 21:55:24 +0900

    カメラ画面について学ぼうとした無理だった📸

--- sotsugyo/CameraView.swift
+++ sotsugyo/CameraView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct PhotoView: View {
+struct CameraView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    PhotoView()
+    CameraView()
 }
