f72faf037f8617c126ea95079e5ab5de04edf68b
sakihosonuma <chipdale411@gmail.com>
2023-11-22 12:23:39 +0900

    一旦プレビュー画面まで😕

---
+++ sotsugyo/DrawingView.swift
@@ -0,0 +1,18 @@
+//
+//  DrawingView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/11/20.
+//
+
+import SwiftUI
+
+struct DrawingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingView()
+}
