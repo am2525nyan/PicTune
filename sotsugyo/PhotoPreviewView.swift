52820ece9e2632860c9664c81fd2452c8faa54ec
sakihosonuma <chipdale411@gmail.com>
2023-11-23 22:24:05 +0900

    上から文字書けるようになったかも📝

---
+++ sotsugyo/PhotoPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPreviewView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/11/22.
+//
+
+import SwiftUI
+
+struct PhotoPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPreviewView()
+}
