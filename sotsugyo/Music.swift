c32a4c855910d4eaf65bac43a46733eb5380869b
sakihosonuma <chipdale411@gmail.com>
2023-11-27 16:39:51 +0900

    音楽すっごいゆっくり検索してくれます

---
+++ sotsugyo/Music.swift
@@ -0,0 +1,8 @@
+//
+//  Music.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/11/27.
+//
+
+import Foundation
---
+++ sotsugyo/SearchMusicView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchMusicView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/11/27.
+//
+
+import SwiftUI
+
+struct SearchMusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchMusicView()
+}
