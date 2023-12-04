fdf9723e567926a68ac5d598f35c2a7a1938b830
sakihosonuma <chipdale411@gmail.com>
2023-12-04 23:24:31 +0900

    日付表示まで後少し！画像は大きく表示できた！⌚️

---
+++ sotsugyo/View/ImageDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageDetailView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/04.
+//
+
+import SwiftUI
+
+struct ImageDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageDetailView()
+}
