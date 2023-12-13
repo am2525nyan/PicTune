0946c6f3f6b2ee33db0a3aa4ab2babbca0dda53c
sakihosonuma <chipdale411@gmail.com>
2023-12-13 18:49:05 +0900

    フォルダに入れれるようになたよ！📄

---
+++ sotsugyo/View/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/13.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ sotsugyo/View/SignInedView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInedView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/13.
+//
+
+import SwiftUI
+
+struct SignInedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInedView()
+}
