b6358fbf98596e280bc99e55352a950d6649106d
sakihosonuma <chipdale411@gmail.com>
2023-12-16 00:28:19 +0900

    勝手にフォルダ選択変わる以外フォルダ治ったかも！☺️

---
+++ sotsugyo/View/main/FolderContentView.swift
@@ -0,0 +1,18 @@
+//
+//  FolderContentView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/16.
+//
+
+import SwiftUI
+
+struct FolderContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FolderContentView()
+}
---
+++ sotsugyo/View/main/MainImageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainImageView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/16.
+//
+
+import SwiftUI
+
+struct MainImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainImageView()
+}
