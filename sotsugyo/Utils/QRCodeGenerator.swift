c9c98bcc05e76ca9e2e2ad4e8d82ae0b16811c31
sakihosonuma <chipdale411@gmail.com>
2023-12-18 15:27:20 +0900

    QRコードを読み込んで誰と取るかわかるようになったよ！🧑‍💻

---
+++ sotsugyo/Utils/QRCodeGenerator.swift
@@ -0,0 +1,8 @@
+//
+//  QRCodeGenerator.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/18.
+//
+
+import Foundation
---
+++ sotsugyo/View/FriendQRView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendQRView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/18.
+//
+
+import SwiftUI
+
+struct FriendQRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendQRView()
+}
