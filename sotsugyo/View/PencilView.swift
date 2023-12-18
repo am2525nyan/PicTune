3d8bf1f4006ce775eab8d23dd1b0a5817ae3cf47
sakihosonuma <chipdale411@gmail.com>
2023-12-18 13:41:03 +0900

    削除したり削除できないようにできたよ！🗑️

---
+++ sotsugyo/View/PencilView.swift
@@ -0,0 +1,18 @@
+//
+//  PencilView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/18.
+//
+
+import SwiftUI
+
+struct PencilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PencilView()
+}
