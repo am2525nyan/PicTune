dbaaf1d04b7d629c23e1383acfc149718d483469
sakihosonuma <chipdale411@gmail.com>
2023-12-22 22:47:03 +0900

    コード間違えたらfirebase上限達したので動作未確認です😭

---
+++ sotsugyo/Utils/NFCSession.swift
@@ -0,0 +1,8 @@
+//
+//  NFCSession.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/20.
+//
+
+import Foundation
---
+++ sotsugyo/View/main/NFCView.swift
@@ -0,0 +1,18 @@
+//
+//  NFCView.swift
+//  sotsugyo
+//
+//  Created by saki on 2023/12/20.
+//
+
+import SwiftUI
+
+struct NFCView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NFCView()
+}
