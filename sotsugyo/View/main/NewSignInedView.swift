96700698e7c8b2391061125a176f110e072f7810
sakihosonuma <chipdale411@gmail.com>
2024-01-09 08:39:38 +0900

    色はないけどメイン画面これでいいかなーーーー

---
+++ sotsugyo/View/main/NewSignInedView.swift
@@ -0,0 +1,18 @@
+//
+//  NewSignInedView.swift
+//  sotsugyo
+//
+//  Created by saki on 2024/01/08.
+//
+
+import SwiftUI
+
+struct NewSignInedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewSignInedView()
+}
