06349230047d6fa4f1854b74225222ea61dc2182
sakihosonuma <chipdale411@gmail.com>
2024-01-10 12:08:20 +0900

    再認証！！

---
+++ sotsugyo/View/main/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  sotsugyo
+//
+//  Created by saki on 2024/01/09.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
